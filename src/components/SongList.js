82d80799069a86299b04ccfa1199c568805cf265
fapjsc <p82szh@gmail.com>
2021-08-02 00:20:28 +0800

    base

---
+++ src/components/SongList.js
@@ -0,0 +1,35 @@
+import { connect } from 'react-redux';
+
+// Actions
+import { selectSong } from '../actions';
+
+const SongList = ({ songs, selectSong }) => {
+  const selectSongHandler = song => {
+    selectSong(song);
+  };
+
+  // Jsx
+  const songListEl = songs.map(song => (
+    <div className="item" key={song.title}>
+      <div className="right floated content">
+        <button className="ui button primary" onClick={() => selectSongHandler(song)}>
+          Select
+        </button>
+      </div>
+      <div className="content">{song.title}</div>
+    </div>
+  ));
+
+  return <div className="ui divided list">{songListEl}</div>;
+};
+
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    songs: state.songs,
+  };
+};
+
+export default connect(mapStateToProps, {
+  selectSong,
+})(SongList);
